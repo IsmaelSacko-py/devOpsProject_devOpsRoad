2aad4b7347fc8bcdf3d6a850d15238a9ab14a3d7
Ismael SACKO <sakoismael.is.1999@gmail.com>
2024-01-31 01:29:17 +0000

    Ajout d'une fonctionnalité

--- src/main/java/sn/devOpsRoad/Operation.java
+++ src/main/java/sn/devOpsRoad/Operation.java
@@ -15,4 +15,12 @@ public class Operation
     {
         return number1 - number2;
     }
+
+    public int multiplication(int number1, int number2)
+    {
+        return number1 * number2;
+    }
+
+
+
 }

a767a74eaf19bd087a4c7a9d76d46998db70f308
Ismael SACKO <sakoismael.is.1999@gmail.com>
2024-01-31 11:44:17 +0000

    Suppression de la fonction multiplication

--- src/main/java/sn/devOpsRoad/Operation.java
+++ src/main/java/sn/devOpsRoad/Operation.java
@@ -16,11 +16,6 @@ public class Operation
         return number1 - number2;
     }
 
-    public int multiplication(int number1, int number2)
-    {
-        return number1 * number2;
-    }
-
 
 
 }
